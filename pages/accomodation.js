cc7709f4d2cf3428a089df48c6b49aca0efecd7e
horasmpsidabalok@gmail.com <horasmpsidabalok@gmail.com>
2022-12-14 17:28:40 +0700

    hampir finallll

--- pages/accomodation.js
+++ pages/accomodation.js
@@ -2,30 +2,32 @@ import { useEffect } from 'react'
 import catalogsty from '../styles/catalog.module.css'
 import Header from './comp/_header'
 
+import data from '../database.json'
+import Link from 'next/link'
+
 export default function Catalogue(){
 
-    let daftar = [
-        ["sabar", "https://unsplash.it/1600/900"],
-        ["sabar", "https://unsplash.it/1600/901"],
-        ["sabar", "https://unsplash.it/1600/902"],
-        ["sabar", "https://unsplash.it/1600/903"],
-        ["sabar", "https://unsplash.it/1600/904"],        
-        ["sabar", "https://unsplash.it/1600/900"],
-        ["sabar", "https://unsplash.it/1600/901"],
-        ["sabar", "https://unsplash.it/1600/902"],
-        ["sabar", "https://unsplash.it/1600/903"],
-        ["sabar", "https://unsplash.it/1600/904"], 
-        ["sabar", "https://unsplash.it/1600/904"],
-    ]
+    let acdata = data.accomodation
+
+    acdata[0].alamat
+    acdata[0].deskripsi
+    acdata[0].featuredFoto
+    acdata[0].rating
+    acdata[0].links
+    acdata[0].jamBuka
+    acdata[0].nFoto
+    acdata[0].featuredFoto
+    acdata[0].nama
 
     return <div style={{
-        backgroundImage : 'url("https://unsplash.it/1920/1080")',
+        backgroundImage : 'linear-gradient(180deg, #000, rgba(0,0,0,0)), url("https://unsplash.it/1920/1080")',
         backgroundSize: "cover",
     }}>
         <Header addtClass={"fixed-top"} />
             <div style={{
                 backdropFilter: 'blur(10px)',
-                minHeight : "100vh"
+                minHeight : "100vh",
+                padding : "70px 0"
             }}>
             <div className='d-flex' style={{
                 margin : 0,
@@ -34,19 +36,24 @@ export default function Catalogue(){
                 flexWrap : 'wrap',
             }}>
                 {
-                    daftar.map(i => {
+                    acdata.map((i, id) => {
                         return (
                             <div class="col-md-4 col-sm-12 col-lg-3 col-12 row" key={i} id={"c"+i} style={{
                                 padding : 0,
                                 margin : 0,
-                                "background": `linear-gradient( rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) ), url(${i[1]})`,
+                                "background": `linear-gradient( rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) ), url('${data.storageurl}/akomodasi/${i.nama}/${i.featuredFoto}.png')`,
                                 backgroundSize : "cover",
                                 color: "white",
                                 height:"250px",
                             }}>
-                                <div class="col-12 d-flex align-items-center justify-content-left px-5" style={{}} id="aco-1">
-                                    <h3>{i[0]}</h3>
-                                </div>
+                                    <div class="col-12 d-flex align-items-center justify-content-left px-5" style={{}} id="aco-1">
+                                    <Link href={"accomodation/"+id} style={{
+                                        color : 'white',
+                                        textDecoration : 'none'
+                                    }}>
+                                        <h3>{i.nama}</h3>
+                                    </Link>
+                                    </div>
                             </div>   
                         )
                     })
--- pages/comp/_home.js
+++ pages/comp/_home.js
@@ -1,22 +1,13 @@
+import data from "../../database.json"
+
 export default function Home(){
-    let daftar = [
-        {
-            Title : "Test 1",
-            Description : "THIS IS A AHBHASA",
-            BackgroundURL : "https://picsum.photos/1920/1080"
-        },
-        {
-            Title : "Test 2",
-            Description : "THIZ IS FOCK",
-            BackgroundURL : "https://picsum.photos/1366/768"
-        },
-    ]
+    let acdata = data.destination.slice(0, 9)
 
     return <>
                 <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-indicators">
                 {
-                    daftar.map((i, index) => {
+                    acdata.map((i, index) => {
                         if(index == 0){
                             return  (
                                 <button key={index} type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to={index} class="active" aria-current="true" aria-label={"Slide "+(index+1)}></button>
@@ -32,13 +23,14 @@ export default function Home(){
             <div className="carousel-inner">
 
             {
-                daftar.map((i, index) => {
+                acdata.map((i, index) => {
                     return  <div key={index} className={"carousel-item "+((index == 0) ? "active" : "")} style={{
-                                    backgroundImage : `url('${i.BackgroundURL}')`,
+                                    backgroundImage : `linear-gradient(0deg, rgba(0,0,0,.7), rgba(0,0,0,0)), url('${data.storageurl}/destinasi/${i.nama}/${i.featuredFoto}.png')`,
+                                    backgroundSize : 'cover'
                             }}>
                             <div className="carousel-caption">
-                                <h5>{i.Title + " " + index}</h5>
-                                <p>{i.Description}</p>
+                                <h5>{i.nama}</h5>
+                                <p>{i.alamat}</p>
                             </div>
                         </div>
                 })
